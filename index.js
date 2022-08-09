61f0f6a736365a3426bdf0feecd9857acc7ac5b0
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-09 10:06:21 -0700

    added some notes on stacks and queues

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ class Stack {}
 
 class Queue {}
 
+class MyDataStructure {
+  constructor(initialList) {
+    //adding # in front makes this a private variable only accessible in the class itself
+    this.#list = initialList;
+  }
+  //adding this will let us see the private variable it will not work without the get
+  get list() {
+    return this.#list;
+  }
+  static describeClass() {
+    console.log("this is a custom Data Structure");
+  }
+}
+
+const myStructure = new MyDataStructure([1, 2, 3]);
+console;
+
 module.exports = { Stack, Queue };
